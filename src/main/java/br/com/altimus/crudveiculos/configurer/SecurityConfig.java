58e3c479a3ce96360bab3dc81851c3633a5811e2
Caiohgp <caio.102008@gmail.com>
2021-11-18 16:53:12 -0300

    Editando security config

--- src/main/java/br/com/altimus/crudveiculos/configurer/SecurityConfig.java
+++ src/main/java/br/com/altimus/crudveiculos/configurer/SecurityConfig.java
@@ -1,9 +1,14 @@
 package br.com.altimus.crudveiculos.configurer;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -11,12 +16,34 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
+@Log4j2
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable();
+//        http.csrf().disable()
+//                .authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .httpBasic();
         http.cors();
+
     }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        log.info("Password encoded {}", passwordEncoder.encode("123456"));
+//        auth.inMemoryAuthentication()
+//                .withUser("admin")
+//                .password(passwordEncoder.encode("123456"))
+//                .roles("USER", "ADMIN")
+//                .and()
+//                .withUser("user")
+//                .password(passwordEncoder.encode("123456"))
+//                .roles("USER");
+//    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
@@ -26,8 +53,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 "GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 }
